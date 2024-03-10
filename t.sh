45b82987f1b54ad1372c2f1158e7313b51ce79e8
Allen Guo <guosonglin001@gmail.com>
2024-03-10 09:29:25 +0000

    misc: wip
    Change-Id: I20dc9fd35b31e5b7850ec8dafff22e0e607447a8

--- t.sh
+++ t.sh
@@ -1,5 +1,11 @@
 # used in docker env
 
+# for vscode
+cat >>/root/.bashrc <<-EOF
+export PATH=\$PATH:\${EXT_PATH}
+export PYTHONPATH=\$PYTHONPATH:\${EXT_PYTHONPATH}
+EOF
+
 # use clang
 export CC=clang
 export CXX=clang++
@@ -18,19 +24,16 @@ pip install -U scons
 apt install libcapstone-dev
 
 # all
+# scons -j9 build/ALL/gem5.debug
 scons -j9 build/ALL/gem5.opt
 scons build/ALL/compile_commands.json
-
-# # only RISCV
-# scons -j9 build/RISCV/gem5.fast
+# only RISCV
+# scons -j9 build/RISCV/gem5.debug
+# scons -j9 build/RISCV/gem5.opt
 # scons build/RISCV/compile_commands.json
 
 # optional: cp files for python type-hint
 cp build/ALL/python/m5/defines.py src/python/m5/
 cp build/ALL/python/m5/info.py src/python/m5/
-
-# for vscode
-cat >>/root/.bashrc <<-EOF
-export PATH=\$PATH:\${EXT_PATH}
-export PYTHONPATH=\$PYTHONPATH:\${EXT_PYTHONPATH}
-EOF
+# cp build/RISCV/python/m5/defines.py src/python/m5/
+# cp build/RISCV/python/m5/info.py src/python/m5/
