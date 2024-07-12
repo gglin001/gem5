d944259dd43b7a8a4ebef282d1c3bedf58d7bb43
Allen Guo <guosonglin001@gmail.com>
2024-07-12 15:04:29 +0800

    misc: wip
    Change-Id: I12c038d335b12528b18968bc852cb2b53d2accff

--- _demos/t1.build.sh
+++ _demos/t1.build.sh
@@ -14,14 +14,17 @@ EOF
 
 # export CCFLAGS_EXTRA="-I$CONDA_PREFIX/include -I$CONDA_PREFIX/include/python3.11"
 # export CCFLAGS_EXTRA="-I/Library/Developer/CommandLineTools/SDKs/MacOSX14.4.sdk/usr/include"
+# export DYLD_LIBRARY_PATH=/Users/allen/micromamba/envs/pyenv/lib
+# export CCFLAGS_EXTRA="-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX14.4.sdk -L/Users/allen/micromamba/envs/pyenv/lib"
+# export LINKFLAGS="-Wl,-rpath=$CONDA_PREFIX/lib"
+# export RPATH="$CONDA_PREFIX/lib"
+# export CCFLAGS_EXTRA="-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX14.4.sdk"
 
 ###############################################################################
 
 export PYTHON_CONFIG=$(which python3-config)
 export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
-export DYLD_LIBRARY_PATH="$CONDA_PREFIX/lib:$DYLD_LIBRARY_PATH"
 export PKG_CONFIG_PATH="$CONDA_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH"
-export CCFLAGS_EXTRA="-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX14.4.sdk -L/Users/allen/micromamba/envs/pyenv/lib"
 export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 
@@ -29,8 +32,20 @@ TARGET=ARM_RISCV
 # TARGET=ALL
 # TARGET=RISCV
 
+# TODO: better way to solve rpath issue
+# export DYLD_LIBRARY_PATH="$CONDA_PREFIX/lib:$DYLD_LIBRARY_PATH" # not work
+
+# not work
+# mkdir -p build/$TARGET
+# mkdir -p build/$TARGET/gem5.build/scons_config
+# ln -s $CONDA_PREFIX/lib/libc++.1.dylib build/$TARGET/
+# ln -s $CONDA_PREFIX/lib/libpython3.11.dylib build/$TARGET/
+# ln -s $CONDA_PREFIX/lib/libz.1.dylib build/$TARGET/
+# ln -s $CONDA_PREFIX/lib/libc++.1.dylib build/$TARGET/gem5.build/scons_config
+# ln -s $CONDA_PREFIX/lib/libpython3.11.dylib build/$TARGET/gem5.build/scons_config
+
 scons --no-colors -j$(nproc) build/$TARGET/gem5.opt
-scons build/$TARGET/compile_commands.json
+# scons build/$TARGET/compile_commands.json
 
 ###############################################################################
 
@@ -44,7 +59,26 @@ gem5.opt util/gem5-stubgen.py
 
 ###############################################################################
 
-# EXE=build/ARM_RISCV/gem5.build/scons_config/conftest_2176b2d932a60acfa5b4079f6e5ab92b_0_ec29e9f2931517fd22ea214641da1cb9 >build/ARM_RISCV/gem5.build/scons_config/conftest_2176b2d932a60acfa5b4079f6e5ab92b_0_ec29e9f2931517fd22ea214641da1cb9.out
+# EXE=build/ARM_RISCV/gem5.build/scons_config/conftest_2176b2d932a60acfa5b4079f6e5ab92b_0_ec29e9f2931517fd22ea214641da1cb9
 # otool -L $EXE
+# install_name_tool -change "@rpath/libc++.1.dylib" "/Users/allen/micromamba/envs/pyenv/lib/libc++.1.dylib" $EXE
+# install_name_tool -change "@rpath/libpython3.11.dylib" "/Users/allen/micromamba/envs/pyenv/lib/libpython3.11.dylib" $EXE
+# $EXE
+
+###############################################################################
+
+#
+
+EXE=$PWD/build/ARM_RISCV/gem5py
+otool -L $EXE
+install_name_tool -change "@rpath/libc++.1.dylib" "/Users/allen/micromamba/envs/pyenv/lib/libc++.1.dylib" $EXE
+install_name_tool -change "@rpath/libpython3.11.dylib" "/Users/allen/micromamba/envs/pyenv/lib/libpython3.11.dylib" $EXE
+install_name_tool -change "@rpath/libz.1.dylib" "/Users/allen/micromamba/envs/pyenv/lib/libz.1.dylib" $EXE
+
+EXE=$PWD/build/ARM_RISCV/gem5py_m5
+otool -L $EXE
+install_name_tool -change "@rpath/libc++.1.dylib" "/Users/allen/micromamba/envs/pyenv/lib/libc++.1.dylib" $EXE
+install_name_tool -change "@rpath/libpython3.11.dylib" "/Users/allen/micromamba/envs/pyenv/lib/libpython3.11.dylib" $EXE
+install_name_tool -change "@rpath/libz.1.dylib" "/Users/allen/micromamba/envs/pyenv/lib/libz.1.dylib" $EXE
 
 ###############################################################################
